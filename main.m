86ac16b40894009a245c9d42bb6f71498a74aecb
agn27 <agn27@cornell.edu>
2017-04-30 17:13:54 -0400

    remove beat2

--- main.m
+++ main.m
@@ -1,10 +1,5 @@
 [y,L,fs] = read_data('seven.wav');
 
-r = 20;
-rectangle = zeros(L);
-rectangle(1:r+1) = 1; 
-y_half = ifft(Y.*rectangle,1024);
-rectangle(end-r+1:end) = 1;    
-y_rect = ifft(Y.*rectangle,1024);
 
-plot(y_rect)
+
+b = beat2(y,fs);
