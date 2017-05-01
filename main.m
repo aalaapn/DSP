36205316868530b9dc76399f5e44c172ebb49163
agn27 <agn27@cornell.edu>
2017-05-01 12:12:56 -0400

    almost working beat detection

--- main.m
+++ main.m
@@ -1,7 +1,33 @@
 [low, fs] = low_pass('skrill.wav');
-song_len = (size(low)/fs)
-down_low = downsample(low, 10)
-spectrogram(low(:,1), 10)
+song_len = (size(low)/fs);
+down_low = downsample(low, 100);
+%audiowrite('output.wav', down_low, fs/1000);
+% plot(down_low);
+% figure
+% plot(low);
+
+shft_amt = zeros(10,1);
+down_low = down_low(:,1);
+shift_down_low = cat(1, shft_amt, down_low);
+
+cross_cor = xcorr(down_low, shift_down_low);
+% auto_cor = autocorr(down_low);
+[ACF,lags,bounds] = autocorr(down_low,int64(fs/100),[],2);
+
+int64(fs/100)
+plot(ACF)
+
+[~,locs]=findpeaks(ACF);
+lagindex = mean(diff(locs)*0.01);
+
+((fs/10000)*60)/lagindex;
+
+a = [];
+for i = 100:size(ACF)
+    append(a, ACF(i));
+end
+plot(a)
+
 
 % [autocorr, lags] = xcorr(low, fs*10);
 % 
