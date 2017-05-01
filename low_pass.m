e1f0ac76b721747a6991c9856c7592b23f948d9c
lab user <lab user>
2017-04-30 23:10:12 -0400

    philips 303

--- low_pass.m
+++ low_pass.m
@@ -1,7 +1,7 @@
-function low_data  = low_pass(data, fs)
-    [y,fs] = audioread(song);
-    y = y(:,1);
+function [low_data, Fs] = low_pass(song)
+    [St,Fs] = audioread(song);
     
+    St;
     % Fourier transform of the input signal S(t)
     % NFFT = 2^nextpow2(L);                              % Next power of 2 from length of L
     NFFT=2^16;
@@ -13,7 +13,9 @@ function low_data  = low_pass(data, fs)
     [b,a]=ellip(4,0.001,30,[50 500]*2/Fs);
     [H,w]=freqz(b,a,NFFT);
 
-
+    
+    low_data = filter(b,a,St);
+    
 
 
 end
