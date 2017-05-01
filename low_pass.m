4b6db01b1b522a9dbdb26edc68b585328823228b
Aalaap Ganesh Narasipura <agn27@cornell.edu>
2017-05-01 13:20:38 -0400

    11 sunset

--- low_pass.m
+++ low_pass.m
@@ -1,7 +1,6 @@
 function [low_data, Fs] = low_pass(song)
     [St,Fs] = audioread(song);
-    
-    St;
+  
     % Fourier transform of the input signal S(t)
     % NFFT = 2^nextpow2(L);                              % Next power of 2 from length of L
     NFFT=2^16;
@@ -12,6 +11,7 @@ function [low_data, Fs] = low_pass(song)
     % Band filter
     [b,a]=ellip(4,0.001,30,[50 500]*2/Fs);
     [H,w]=freqz(b,a,NFFT);
+
 
     
     low_data = filter(b,a,St);
--- main.m
+++ main.m
@@ -1,4 +1,5 @@
-[low, fs] = low_pass('skrill.wav');
+tic
+[low, fs] = audioread('120.mp3');
 song_len = (size(low)/fs);
 down_low = downsample(low, 100);
 %audiowrite('output.wav', down_low, fs/1000);
@@ -14,7 +15,7 @@ cross_cor = xcorr(down_low, shift_down_low);
 % auto_cor = autocorr(down_low);
 [ACF,lags,bounds] = autocorr(down_low,int64(fs/100),[],2);
 
-int64(fs/100)
+
 plot(ACF)
 
 [~,locs]=findpeaks(ACF);
@@ -23,20 +24,27 @@ lagindex = mean(diff(locs)*0.01);
 ((fs/10000)*60)/lagindex;
 
 a = [];
-for i = 100:size(ACF)
-    append(a, ACF(i));
+for i = 100:400
+    a = [a ACF(i)];
 end
-plot(a)
 
+maxi = max(a);
 
-% [autocorr, lags] = xcorr(low, fs*10);
+ind = find(ACF==maxi)
+
+BPM = fs/100*60/ind
+ind = fs/100*60/120
+
+% fs1 = fs/100;
+% [s1,f,t,p1]=spectrogram(down_low,hann(8192));
+% plot(p1)
+% % sf1=10*log10(geomean(p1)./mean(p1)); % spectral flatness
+% % plot(linspace(0,length(down_low)/fs1,length(sf1)),sf1); axis tight
 % 
-% 
-% 
-% [pks,loc] = findpeaks(autocorr(:,1));
-% 
-% 
-% short = mean(diff(loc))/fs
-% [pklg,lclg] = findpeaks(autocorr(:,1), ...
-%     'MinPeakDistance',ceil(short)*fs,'MinPeakheight',0.3);
-% long = mean(diff(lclg))/fs
+% pxx = periodogram(a);
+% %figure(2)
+% %plot(pxx)
+% num=geomean(pxx);
+% den=mean(pxx);
+% spf=num/den ;
+toc
